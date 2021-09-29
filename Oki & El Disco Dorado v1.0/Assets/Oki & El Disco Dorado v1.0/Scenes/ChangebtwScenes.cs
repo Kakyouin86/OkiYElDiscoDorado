0a5ffbf76e885ac76a41b60b210a2fe870a83f73
Leonardo Jorge Monge <leo.monge1986@gmail.com>
2021-09-28 22:02:52 -0300

    1

---
+++ Oki & El Disco Dorado v1.0/Assets/Oki & El Disco Dorado v1.0/Scenes/ChangebtwScenes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangebtwScenes : MonoBehaviour
+{
+
+    public string sceneName;
+
+
+
+
+    void ChangeScene()
+
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
+
